f1afb503b2c946492aa3e780d3e487551a4d0a58
RahulMahawar310 <rahul02.bmahawar@gmail.com>
2025-04-08 21:56:47 +0530

    News-Website

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-// import logo from './logo.svg'
-import './index.css'
-import './App.css'
+// import logo from './logo.svg';
+import './index.css';
+import './App.css';
 import Newsapp from './Components/Newsapp'
 
 function App() {
   return (
     <Newsapp/>
-  )
+  );
 }
 
 export default App
