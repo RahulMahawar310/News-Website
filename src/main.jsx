0cdd0df7373c5443dc69d7e87a5cf68d6a3cc07a
Rahul Mahawar <rahul02.bmahawar@gmail.com>
2025-04-10 09:28:20 +0530

    Update and rename main.js to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App.js';
+import App from './App.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
